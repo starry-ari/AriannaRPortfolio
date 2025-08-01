e8cea8ccf1308e130ffb9c5beb817827943d4ad5
Arianna <44093846+starry-ari@users.noreply.github.com>
2025-08-01 11:44:06 -0400

    test

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,13 @@ load_dotenv()
 
 
 # Peewee DB setup
+mydb = MySQLDatabase(
+    os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
 
 
 # Peewee model
