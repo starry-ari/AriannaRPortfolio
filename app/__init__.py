886998150c52fe8930219820814339df421ba12e
Arianna <44093846+starry-ari@users.noreply.github.com>
2025-08-01 12:00:32 -0400

    test3

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ mydb = MySQLDatabase(
     os.getenv("MYSQL_DATABASE"),
     user=os.getenv("MYSQL_USER"),
     password=os.getenv("MYSQL_PASSWORD"),
-    host=os.getenv("MYSQL_HOST"),
+    host=os.getenv("MYSQL_HOST", "localhost"),
     port=3306
 )
 
