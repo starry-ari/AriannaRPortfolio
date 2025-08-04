c4a446a5b8d67e145b27b5c52a81ef353164f51c
Arianna <44093846+starry-ari@users.noreply.github.com>
2025-08-04 17:42:40 -0400

    docker

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 tmux kill-server
+python3 -m venv venv
 
-cd AriannaRportfolio
+cd /root/AriannaRPortfolio
 
 git fetch && git reset origin/main --hard
 
-source python3-virtualvenv/bin/activate 
+source venv/bin/activate
 
 pip install -r requirements.txt
 
