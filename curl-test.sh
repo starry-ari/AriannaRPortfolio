47b35c04a21c4a3962fed34835942201e4656569
Arianna <44093846+starry-ari@users.noreply.github.com>
2025-08-04 12:49:11 -0400

    curl

--- curl-test.sh
+++ curl-test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-curl -X POST http://localhost:5000/api/timeline_post \
-     -H "Content-Type: application/json" \
-     -d '{"name": "Arianna R", "email":arianna.richardson2020@gmail.com"
-        "content": "This is a test post from curl!"}'
 
-curl -X GET http://localhost:5000/api/timeline_post
-        {"timeline_posts": []}
-        
+curl --request POST http://localhost:5000/api/timeline_post \
+  -d "name=Wei" \
+  -d "email=wei.he@mlh.io" \
+  -d "content=Just Added Database to my portfolio site!"
+
+
+curl --request POST http://localhost:5000/api/timeline_post \
+  -d "name=Wei" \
+  -d "email=wei.he@mlh.io" \
+  -d "content=Testing my endpoints with postman and curl."
+
+
+curl http://localhost:5000/api/timeline_post
